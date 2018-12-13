a5e533bf603d8d47d84d05112a0419c10a404a6f
ericmagallan <ericmagallan85@gmail.com>
2018-12-12 19:29:03 -0800

    Forgot to save logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,5 @@
 // Array of strings of topics that interest me
 var nickCartoons = [Doug, Hey Arnold, Rugrats, Aahh Real Monsters, Wild Thornberries]
 
-//
+//This portion of the code will have the remaining functions of the app, including clicking a portion that creates buttons in the html (DOM).
+// 
